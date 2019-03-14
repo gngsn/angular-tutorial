96144a2095778ef8c2278764ff4d784dd2922bf5
tomas4540 <rudtjs4540@gmail.com>
2019-03-15 01:12:08 +0900

    make component files

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id : 11, name: 'Nick'},
+  { id : 12, name: 'Narco'},
+  { id : 13, name: 'Dr.IQ'},
+  { id : 14, name: 'ganada'},
+  { id : 15, name: 'jinsu'},
+  { id : 16, name: 'gngsn'},
+  { id : 17, name: 'hammm'}
+];
